92a7367d001e4ed39f7d5e833dc3bf4eb7d5ff0a
Alan Race <alan.m.race@gmail.com>
2018-10-31 11:08:41 +0100

    Tidied up Display class and added more comments

--- gui/Display.m
+++ gui/Display.m
@@ -1,29 +1,38 @@
 classdef Display < handle
     properties (SetAccess = private)
-        data;
+        data; % Data to be displayed
         
-        parent;
-        axisHandle;
+        parent; % Parent of the display, either Figure or Panel
+        axisHandle; % Axis handle used to display the data
     end
     
     properties (Access = protected)
-        dataListener;
+        % dataListener is listener that is added to the data and is
+        % triggered each time the data is updated. This is deleted and renewed
+        % each time the data is changed.
+        dataListener; 
         
-        contextMenu;
-        exportMenu;
+        contextMenu; % Context menu stored on <b>parent</b>
+        exportMenu; % Context sub-menu storing details about exporting data
         
-        lastSavedPath = '';
+        lastSavedPath = ''; % The last used path for saving data to the harddisk
     end
 
     events
-        % 
+        % DisplayChanged is triggered when the display is updated (i.e. the
+        % view is changed somehow)
         DisplayChanged;
     end
     
     methods
         function obj = Display(parent, data)
-%             if(isempty(axisHandle) || ~(ishandle(axisHandle) && strcmp(get(axisHandle, 'Type'), 'axes')))
-
+            % Display creates a display for the <i>data</i> in the
+            % <i>parent</i> Figure or Panel. If the supplied parent is
+            % anything other than Figure or Panel then en exception is
+            % thrown.
+            %
+            % Axis and context menu are created and the data is displayed.
+            
             if(isempty(parent) || (~isa(parent, 'Figure') && ~isa(parent, 'Panel')))
                 exception = MException('Display:invalidArgument', '''parent'' must be a valid instance of Figure or Panel');
                 throw(exception);
@@ -33,22 +42,25 @@ classdef Display < handle
             parentHandle = parent.handle;
             
             obj.axisHandle = axes('Parent', parentHandle);
-%             set(obj.axisHandle, 'ActivePositionProperty', 'OuterPosition');
             
             obj.setData(data);
             
             obj.createContextMenu();
             
             % Set up callbacks
-            %set(get(obj.axisHandle, 'Parent'), 'UIContextMenu', obj.contextMenu);
             set(obj.axisHandle, 'UIContextMenu', obj.contextMenu);
         end
         
         function data = getData(this)
+            % getData returns the raw data being displayed
+            
             data = this.data;
         end
         
         function setData(obj, data)
+            % setData sets the specified data to be displayed and updates
+            % the display.
+            
             if(~isa(data, 'Data'))
                 exception = MException('Display:invalidArgument', 'Must provide an instance of a class that extends Data');
                 throw(exception);
@@ -65,12 +77,17 @@ classdef Display < handle
         end
         
         function createContextMenu(obj)
-            parentHandle = obj.parent.getParentFigure().handle; %parent.handle;%
+            % createContextMenu creates a context menu on the <b>parent</b>
+            % of the Display. This overrides any existing context menu.
+            
+            parentHandle = obj.parent.getParentFigure().handle;
             
             % Set up the context menu
             obj.contextMenu = uicontextmenu('Parent', parentHandle);
-            openInNewWindow = uimenu(obj.contextMenu, 'Label', 'Open in new window', 'Callback', @(src,evnt)obj.openInNewWindow());
-            openCopyInNewWindow = uimenu(obj.contextMenu, 'Label', 'Open copy in new window', 'Callback', @(src,evnt)obj.openCopyInNewWindow());
+            uimenu(obj.contextMenu, 'Label', 'Open in new window', 'Callback', @(src,evnt)obj.openInNewWindow());
+            uimenu(obj.contextMenu, 'Label', 'Open copy in new window', 'Callback', @(src,evnt)obj.openCopyInNewWindow());
+            
+            % Set up export menu
             obj.exportMenu = uimenu(obj.contextMenu, 'Label', 'Export Data', 'Callback', []);
             
             if(~isdeployed())
@@ -83,57 +100,45 @@ classdef Display < handle
         end
         
         function exportToMAT(this)
+            % exportToMAT exports the data to a .mat file
+            
             [fileName, pathName, filterIndex] = uiputfile([this.lastSavedPath 'data.mat'], 'Export data');
             
             if(filterIndex > 0)
                 this.lastSavedPath = [pathName filesep];
                 
-                dataToExport = this.data;
-                
-                save([pathName filesep fileName], 'dataToExport');
+                save([pathName filesep fileName], 'this.data');
             end
         end
         
         function disableContextMenu(this)
+            % disableContextMenu deletes and removes the context menu
+            
             delete(this.contextMenu);
             
             this.contextMenu = [];
         end
         
-        
         function updateDisplay(this)
+            % updateDisplay should be overriden by subclasses to update the
+            % display. Display.updateDisplay simply notifies listeners of
+            % the DisplayChanged event
+            
             notify(this, 'DisplayChanged');
         end
     end
     
     methods (Abstract)
+        % openInNewWindow opens the data in a new window, copying any 
+        % display settings.
         openInNewWindow(obj);
+        % openCopyInNewWindow makes a copy of the data and opents this in 
+        % a new window, copying any display settings.
         openCopyInNewWindow(obj);
         
+        % exportToImage exports the visualised data to a PDF image
         exportToImage(obj);
+        % exportToLaTeX exports the visualised data to LaTeX
         exportToLaTeX(obj);
     end
-    
-%     methods (Access = protected)
-%         function buttonDownCallback(obj)
-%             currentPoint = get(obj.axisHandle, 'CurrentPoint');
-%             
-%             xLimit = get(obj.axisHandle, 'xLim');
-%             yLimit = get(obj.axisHandle, 'yLim');
-%             
-%             xPoint = currentPoint(1, 1);
-%             yPoint = currentPoint(1, 2);
-%             
-%             mouseClick = get(get(obj.axisHandle, 'Parent'), 'SelectionType');
-%             
-%             mouseEvent = MouseEventData(MouseEventData.ButtonDown, xPoint, yPoint);
-%             if(strcmp(mouseClick, 'normal'))
-%                 mouseEvent.setButton(MouseEventData.LeftButton);
-%             elseif(strcmp(mouseClick, 'alt'))
-%                 mouseEvent.setButton(MouseEventData.RightButton);
-%             end
-%             
-%             notify(obj, 'MouseDownInsideAxis', mouseEvent);
-%         end
-%     end
 end
